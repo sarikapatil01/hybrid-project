529d2058f6f7263cddff2de200e2e0230d6a96a8
sarikapatil01 <126934371+sarikapatil01@users.noreply.github.com>
2023-07-14 14:52:55 +0530

    Add files via upload

---
+++ src/testPackage/InvalidLoginTestCase.java
@@ -0,0 +1,30 @@
+package testPackage;
+
+import java.io.IOException;
+
+import org.apache.poi.EncryptedDocumentException;
+import org.testng.annotations.Test;
+
+import genericPackage.BaseTest;
+import genericPackage.Flib;
+import pagePackage.LoginPage;
+
+public class InvalidLoginTestCase extends BaseTest{
+
+	@Test
+	public void inavlidLogin() throws IOException, EncryptedDocumentException, InterruptedException
+	{
+       
+		
+		LoginPage lp = new LoginPage(driver);
+		
+		Flib flib = new Flib();
+		int rc = flib.rowCount(EXCEL_PATH,"Invalidcreads");
+		
+		for(int i=1;i<rc;i++)
+		{
+		lp.inValidLogin(flib.readExcelData(EXCEL_PATH,"Invalidcreads",i, 0),flib.readExcelData(EXCEL_PATH,"Invalidcreads",i,1));
+		}
+	}
+
+}
